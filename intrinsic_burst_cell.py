3e583fd18e6ad94de78cff93e9bf7ede40a12141
JonathanJBarker <87146655+JonathanJBarker@users.noreply.github.com>
2021-07-14 17:37:40 -0500

    Create intrinsic_burst_cell.py
    I completed this activity however I do not know how to submit

---
+++ intrinsic_burst_cell.py
@@ -0,0 +1,22 @@
+import izhikevich_cells as izh
+
+class izhCell(izh.izhCell):
+    def __init__(self,stimVal):
+        super().__init__(stimVal)
+        self.celltype='Intrinzic Burst' # Regular spiking
+        self.C=150
+        self.vr=-75
+        self.vt=-45
+        self.k=1.2
+        self.a=0.01
+        self.b=5
+        self.c=-56
+        self.d=130
+        self.vpeak=50
+
+myCell = izhCell(4000)
+myCell.simulate()
+
+if __name__=='__main__':
+    izh.plotMyData(myCell)
+
